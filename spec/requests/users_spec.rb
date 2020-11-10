ea54397a7275fd90163643a9fad7884bd836c3b0
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2020-11-10 19:30:33 -0300

    Refactor affected unit tests by Devise gem

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+
   describe "GET /users" do
-    it "works! (now write some real specs)" do
+    it " without login need to be redirect to root" do
       get users_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
+      expect(response.location).to eq("http://www.example.com/users/sign_in")
     end
   end
 end
